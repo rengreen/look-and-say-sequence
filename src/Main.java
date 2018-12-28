841205e87a5c63dd2f5127c5fbfcaa3be9217d41
rengreen <r.m.gardzinska@gmail.com>
2018-12-28 21:55:38 +0100

    addding loop for n lines

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,25 @@
 public class Main {
 
     public static void main(String[] args) {
+        int n=10;
         String line="11222";
         System.out.println(line);
 
-        int len=line.length();
-        String nextLine="";
-        int counter=1;
-        for (int k=0; k<len; k++){
-            if (k+1<len && line.charAt(k)==line.charAt(k+1)){
-                counter++;
-            } else {
-                nextLine+=counter;
-                nextLine+=line.charAt(k);
-                counter=1;
+        for (int i=0; i<n; i++){
+            int len=line.length();
+            String nextLine="";
+            int counter=1;
+            for (int k=0; k<len; k++){
+                if (k+1<len && line.charAt(k)==line.charAt(k+1)){
+                    counter++;
+                } else {
+                    nextLine+=counter;
+                    nextLine+=line.charAt(k);
+                    counter=1;
+                }
             }
+            System.out.println(nextLine);
+            line=nextLine;
         }
-        System.out.println(nextLine);
     }
 }
