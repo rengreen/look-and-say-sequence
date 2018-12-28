38b63c8da5e0efcf1a111ca5f6d463297c64fc7d
rengreen <r.m.gardzinska@gmail.com>
2018-12-28 21:44:20 +0100

    creating next line

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
 
     public static void main(String[] args) {
+        String line="11222";
+        System.out.println(line);
 
+        int len=line.length();
+        String nextLine="";
+        int counter=1;
+        for (int k=0; k<len; k++){
+            if (k+1<len && line.charAt(k)==line.charAt(k+1)){
+                counter++;
+            } else {
+                nextLine+=counter;
+                nextLine+=line.charAt(k);
+                counter=1;
+            }
+        }
+        System.out.println(nextLine);
     }
 }
